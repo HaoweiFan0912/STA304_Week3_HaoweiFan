05656ec44a6c91005ad79bbc39703c886b266859
HaoweiFan0912 <fhw0912@gmail.com>
2024-09-19 18:19:45 -0400

    My code

---
+++ HaoweiFan/scripts/00-simulate_data.R
@@ -0,0 +1,39 @@
+#### Preamble ####
+# Purpose: Simulates data
+# Author: Haowei Fan
+# Date: 19 September 2024
+# Contact: haowei.fan@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: None
+# Any other information needed? None
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Simulate data ####
+set.seed(304)
+
+start_date <- as.Date("2015-01-01")
+end_date <- as.Date("2020-12-31")
+
+number_of_dates <- 100
+
+data <-
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_marriage = rpois(n = number_of_dates, lambda = 15)
+  )
+
+
+#Write_csv
+write_csv(data, file = "~/Desktop/STA304_Week3/HaoweiFan/data/raw_data/simulated.csv")
+
---
+++ HaoweiFan/scripts/03-test_data.R
@@ -0,0 +1,27 @@
+#### Preamble ####
+# Purpose: Sanity check of the data
+# Author: Haowei Fan
+# Date: 19 September 2024
+# Contact: haowei.fan@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Need to have simulated data
+# Any other information needed? None.
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Test data ####
+data <- read_csv("~/Desktop/STA304_Week3/HaoweiFan/data/raw_data/simulated.csv")
+
+# Test if any value in number_of_marriage is greater than 99999
+any(data$number_of_marriage > 99999)
+
+# Test if all values in number_of_marriage are integers
+all(data$number_of_marriage != floor(data$number_of_marriage))
+
+
+
+
+
